f2bdaec259170b67f62ad799575d252ed2dd70dd
DIFU-N <chukwudifunsofor@gmail.com>
2022-10-31 13:36:23 +0100

    deleting unresponsive countries didn't work

--- script.js
+++ script.js
@@ -1,13 +1,19 @@
 import { apiKey } from "./apiKey.js";
 let ndate = new Date();
 let current_date = '2022-10-01'
-let selectedValues;
-// ndate.getFullYear()+"-"+(ndate.getMonth()+1)+"-"+ ndate.getDate(); 
+ndate.getFullYear()+"-"+(ndate.getMonth()+1)+"-"+ ndate.getDate(); 
 console.log(current_date);
 document.getElementById("today").innerHTML = current_date;
-let notInApi = [];
 
-async function holiday(i, country, thisName) {
+
+let selectedValues;
+
+
+const cNamesForArray = document.querySelector(".country-names").children;
+const cNames = document.querySelector(".country-names"); // this works for getting all the text values from the html class
+const sArray = Array.from(cNamesForArray);
+
+async function holiday(i, country, thisName, htmlSelect) {
 	try {
 		const options = {
 			method: 'GET',
@@ -19,16 +25,17 @@ async function holiday(i, country, thisName) {
 		
 		const response = await fetch('https://public-holiday.p.rapidapi.com/2022/'+country, options);
 		await response.json();
-		checkIfThingIsThere(response);
+		todayIsAHoliday(response);
 
 		document.getElementById("country-names-kdj").innerHTML = selectedValues;
-		//including  all countries*****
+		inApi.push(thisName);
+		//including all countries*****
 			
 	} catch (error) {
-		console.log(thisName);
-		notInApi.push(thisName);
-	}
-
+		notInApi.push(htmlSelect);
+	}	
+	deleteThem(notInApiArray, sArrayOuterHtml);
+	console.log(notInApiArray.length);
 }
 //json path starting with 0
 
@@ -45,24 +52,13 @@ function todayIsAHoliday (response) {
 	}
 }
 
-
-// var ndate = new Date();
-// var current_date = ndate.getFullYear()+"-"+(ndate.getMonth()+1)+"-"+ ndate.getDate();
-// document.getElementById("today").innerHTML = current_date;
-
-
-const cNamesForArray = document.querySelector(".country-names").children;
-const cNames = document.querySelector(".country-names"); // this works for getting all the text values from the html class
-const sArray = Array.from(cNamesForArray);
-
-
 function search() {
 	console.log(document.querySelector(".country-names").value);
 	 selectedValues = [].filter
 	.call(document.querySelector(".country-names").options, option => option.selected)
 	.map(option => option.text);
-	return	testForCountries();
-	// return holiday(document.querySelector(".country-names").value);
+	// return	testForCountries();
+	return holiday(document.querySelector(".country-names").value);
 }
 search();	
 
@@ -81,57 +77,58 @@ ent.addEventListener("keypress", function(event) {
         event.preventDefault();
         document.getElementById('btn').click();
     }
-})
+});
 
 
 
-// looping the select options to see which one returns an error 
-function testForCountries() {
-	// console.log(cNames[1].value); // this helps to get the individual countries from the class, using an array didn't work.
-	document.getElementById("answer").textContent = sArray.length + " countries in total";
 
-	for (let i = 0; i < 20; i++) {
-		let cNamesValue = cNames[i].value;
-		let cNamesText = cNames[i].textContent;
-		holiday(i, cNamesValue, cNamesText);
-	}
-}
-// testForCountries();
 
-function checkIfThingIsThere (response) {
-	// CHECKS IF country response is empty and returns the country name
-	if (response.ok) {
-		console.log("miki");
-	}
-	return null;
-} 
 
-// function checkIfThingIsThere(response) {
-// 	let s = document.querySelector(".country-names").children;
-// 	let sArray = Array.from(s);
-// 	for (let i = 0; i < sArray.length; i++) {
-// 		// console.log(sArray.length);
-// 		// checkEveryCountry = () => {
-// 			if (holiday(sArray[i].value) === null) {
-// 				console.log('mlki');
-// 				if(Object.keys(response.data).length){
-// 					document.getElementById("answer").textContent = sArray[i].outerHTML;
-// 					i++;
-// 					return
-// 				}
-// 				// i++;
-// 				// return checkEveryCountry();
-// 			}	
-// 		// }
-// 		return null;
-// 	}
 
-	// function isEmpty(obj) {
-	// 	for(var prop in obj) {
-	// 		if(obj.hasOwnProperty(prop))
-	// 			return false;
-	// 	}
-	// 	return true;
-	// }
-	// console.log(s[1].value);
+
+
+
+
+
+
+
+// deleting the countries that don't have responses in the API from the HTML select element
+
+// const notInApi = [],
+// 	inApi = [];
+// const notInApiArray = notInApi;
+// console.log(notInApiArray);
+// let sArrayOuterHtml = [];
+// for (let i = 0; i < sArray.length; i++) {
+// 	sArrayOuterHtml.push(sArray[i].outerHTML);
 // }
+// console.log(sArray);
+
+// // looping the select options to see which one returns an error 
+// function testForCountries() {
+// 	// console.log(cNames[1].value); // this helps to get the individual countries from the class, using an array didn't work.
+// 	document.getElementById("answer").textContent = sArray.length + " countries in total";
+
+// 	for (let i = 0; i < sArray.length; i++) {
+// 		let cHTML = sArray[i].outerHTML;	
+// 		let cNamesValue = cNames[i].value;
+// 		let cNamesText = cNames[i].textContent;
+// 		holiday(i, cNamesValue, cNamesText, cHTML);
+// 	}	
+// }
+
+// function deleteThem(deleteThese, fromThese) {
+// 	//check if the cNamesText is in the Notinapi array
+// 	// finds all the elements of arr2 that are not in arr1
+// 	let errorElement = document.querySelector("#error");
+// 	let deleteObjects = deleteThese.filter(val => fromThese.find( fromTheseObj => fromTheseObj.prop1 === val))
+// 	if(deleteObjects){
+// 			errorElement.innerHTML = `
+// 			${deleteThese} 
+// 		`
+// 	}
+// }
+
+// document.querySelector("#delCountry").addEventListener("click", function(e) {
+// 	e.preventDefault();
+// })
